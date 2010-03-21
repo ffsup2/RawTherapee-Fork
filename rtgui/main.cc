e7afcd26212d900f7f10a5cea82c367366771982
TheBigUno <the.big.uno@gmail.com>
2010-03-21 12:19:42 -0600

    Reverted commit 88b53b53d26a59fa80cec01a585614923151c243 which apparently causes problems when running on paths with special characters; please confirm if this helps

--- rtgui/main.cc
+++ rtgui/main.cc
@@ -82,8 +82,8 @@ int main(int argc, char **argv)
     else
         argv1_ = "";
 
-   argv0 = Glib::filename_to_utf8 (argv0_);
-   argv1 = Glib::filename_to_utf8 (argv1_);
+   argv0 = Glib::locale_to_utf8 (argv0_);
+   argv1 = Glib::locale_to_utf8 (argv1_);
 
    Glib::thread_init();
    gdk_threads_init();
